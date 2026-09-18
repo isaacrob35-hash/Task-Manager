35e19a276d1d03d3c521b936a70db1de38ad560a
isaacluke <isaacluke@cedarville.edu>
2026-09-18 12:04:22 -0400

    Initial Commit

---
+++ Task Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task Manager
+//
+//  Created by Isaac Luke on 9/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task Manager/Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagerApp.swift
+//  Task Manager
+//
+//  Created by Isaac Luke on 9/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
